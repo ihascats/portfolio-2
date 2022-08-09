934d606a5d2dd6034e60be180afd6b000bb45138
ihascats <s.pavlovic.rs@gmail.com>
2022-08-09 19:53:23 +0200

    Add a about section, sticky header / nav

--- src/App.js
+++ src/App.js
@@ -34,7 +34,17 @@ function App() {
           </h3>
         </div>
       </section>
-      <section className="about"></section>
+      <section className="about">
+        <div>
+          <h1>About Me</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{' '}
+          </p>
+        </div>
+      </section>
       <section className="projects"></section>
       <section className="contact"></section>
       <footer></footer>
