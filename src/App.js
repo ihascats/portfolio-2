463a75c5e893149c29b9721c39b07690a6f5b26b
ihascats <s.pavlovic.rs@gmail.com>
2022-08-09 17:26:39 +0200

    Add page elements, style nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <button>About</button>
+            </li>
+            <li>
+              <button>Projects</button>
+            </li>
+            <li>
+              <button>Contact</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section className="introduction"></section>
+      <section className="about"></section>
+      <section className="projects"></section>
+      <section className="contact"></section>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
